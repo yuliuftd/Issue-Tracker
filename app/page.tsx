058da82c2cdc19877e17a90a72b094f709d70fa9
liuyuftd <liuyuftd@gmail.com>
2024-05-31 16:30:51 +1000

    set up Radix UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import { Button } from "@radix-ui/themes";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      Hello World
+      <div>
+        <Button>Click</Button>
+      </div>
+    </div>
+  );
 }
