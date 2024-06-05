067029002531db69f295d4697064b473829f249d
liuyuftd <liuyuftd@gmail.com>
2024-06-05 12:10:32 +1000

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import "easymde/dist/easymde.min.css";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import { useRouter } from "next/navigation";
+import ErrorMessage from "../../component/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -46,11 +47,7 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root size="3" placeholder="Title" {...register("title")} />
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -58,11 +55,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
