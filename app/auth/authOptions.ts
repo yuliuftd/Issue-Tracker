66bc46be676e0fd50b1574a83c92a91808c5da6e
liuyuftd <liuyuftd@gmail.com>
2024-08-15 21:10:13 +1000

    increase the timeout for google auth

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,19 +1,22 @@
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/prisma/prisma';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/prisma";
 import { NextAuthOptions } from "next-auth";
 
 const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    session:{
-        strategy:'jwt'
-    }  
-}
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      httpOptions: {
+        timeout: 10000, // Increase the timeout to 10 seconds
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
 
 export default authOptions;
