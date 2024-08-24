954d4cbe72e4b48b72441bdc47e315b2a5dabd9d
liuyuftd <liuyuftd@gmail.com>
2024-08-24 16:30:10 +1000

    Build the issue summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,52 @@
+import prisma from "@/prisma/prisma";
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const containers: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    {
+      label: "Open Issues",
+      value: open,
+      status: "OPEN",
+    },
+    {
+      label: "In Progress Issues",
+      value: inProgress,
+      status: "IN_PROGRESS",
+    },
+    {
+      label: "Closed Issues",
+      value: closed,
+      status: "CLOSED",
+    },
+  ];
+  const issuse = prisma.issue.findMany({});
+  return (
+    <Flex gap={"5"}>
+      {containers.map((x) => (
+        <Card key={x.label}>
+          <Flex direction={"column"} align={"center"} gap={"1"}>
+            <Link href={`/issues/list?status=${x.status}`}>{x.label}</Link>
+            <Text size={"5"} weight={"bold"}>
+              {x.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { Flex } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/prisma";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return (
+    <Flex direction="column" gap="5">
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues />
+    </Flex>
+  );
 }
