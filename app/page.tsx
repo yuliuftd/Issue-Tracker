d303b62a721deec6273310ba883884ba1a95dc2d
liuyuftd <liuyuftd@gmail.com>
2024-08-24 22:16:20 +1000

    Layou the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Grid } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/prisma";
@@ -11,10 +11,12 @@ export default async function Home() {
   });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <Flex direction="column" gap="5">
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-      <IssueChart open={open} inProgress={inProgress} closed={closed} />
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
       <LatestIssues />
-    </Flex>
+    </Grid>
   );
 }
